488437b1309907655e577be8d3e572639ec9d686
SedikitMods <83308371+SedikitMods@users.noreply.github.com>
2021-06-28 08:57:37 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var btn = document.querySelector('.btn');
 var input = document.querySelectorAll('input');
-sudoku = [];
+var sudoku = [];
 
 function checkLength(elem) {
     if (elem.value.length > 1) {
@@ -9,11 +9,35 @@ function checkLength(elem) {
 }
 
 btn.addEventListener('click', () => {
-    for (let i = 0; i < input.length; i++){
+    for(let i = 0; i < input.length; i++){
         sudoku[i] = input[i].value;
         if(input[i].value == ""){
             input[i].placeholder = "0";
         }
     }
-    document.querySelector('.out').innerHTML = sudoku.join(' / ');
+    strokeFind(sudoku);
 });
+
+function strokeFind(list) {
+    for(let j = 0; j < 81; j += 9) {
+        let stroke = [];
+        for(let i = 0; i < 9; i++) {
+            if(list[j+i] != "") {
+                stroke.push(list[j+i]);
+            } else {
+                stroke.push("a");
+            }
+        }
+
+        let need =  [].concat(stroke);
+        need.sort();
+        if (need[need.length - 2] != "a") {
+            for(let k = 0; k < 9; k++) {
+                if(need[k] != String(k+1)) {
+                    sudoku[j+stroke.indexOf("a")] = String(k+1);
+                    input[j+stroke.indexOf("a")].value = String(k+1);
+                }
+            }
+        }
+    }
+}

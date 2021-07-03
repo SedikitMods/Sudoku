18803a8b09c83cd48ae7a40b6630888d8fb48de7
SedikitMods <83308371+SedikitMods@users.noreply.github.com>
2021-07-04 00:08:59 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,15 +9,14 @@ function checkLength(elem) {
 }
 
 btn.addEventListener('click', () => {
-    for(let i = 0; i < input.length; i++){
+    for(let i = 0; i < input.length; i++) {
         sudoku[i] = input[i].value;
-        if(input[i].value == "") {
-            input[i].placeholder = "0";
-        }
     }
+
     strokeFind(sudoku);
     columnFind(sudoku);
     squareFind(squareCreate(sudoku));
+    chanceFind(sudoku);
 });
 
 function strokeFind(list) {
@@ -99,6 +98,69 @@ function squareFind(list) {
                     input[(((list[i].indexOf("") - list[i].indexOf("") % 3) / 3) + (((i - i % 3) / 3) * 3)) * 9 + (((i * 3) % 9) + (list[i].indexOf("") % 3))].value = String(k+1);
                     break;
                 }
+            }
+        }
+    }
+}
+
+function chanceFind(list) {
+    for (let cell = 0; cell < 81; cell++){
+        if (sudoku[cell] == "") {
+            let chance = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            let strokeGood = ["", "", "", "", "", "", "", "", ""];
+            let columnGood = ["", "", "", "", "", "", "", "", ""];
+            let squareGood = ["", "", "", "", "", "", "", "", ""];
+    
+            let stroke = [];
+            for(let i = 0; i < 9; i++) {
+                if(list[(cell - cell % 9) + i] != "") {
+                    stroke.push(list[(cell - cell % 9) + i]);
+                } else {
+                    stroke.push("");
+                }
+    
+                if (stroke[i] != "") {
+                    strokeGood[stroke[i] - 1] = stroke[i];
+                }
+            }
+    
+            let column = [];
+            for(let j = 0; j < 81; j += 9) {
+                if(list[j + cell % 9] != "") {
+                    column.push(list[j + cell % 9]);
+                } else {
+                    column.push("");
+                }
+    
+                if (column[j / 9] != "") {
+                    columnGood[column[j / 9] - 1] = column[j / 9];
+                }
+            }
+    
+            let separated = squareCreate(sudoku);
+            let square = separated[(((cell % 9) - (cell % 9) % 3) / 3) + (((((cell - cell % 9) / 9) - ((cell - cell % 9) / 9) % 3) / 3) * 3)];
+    
+            for(let s = 0; s < 9; s++) {
+                if (square[s] != "") {
+                    squareGood[square[s] - 1] = square[s];
+                }
+            }
+    
+            for(let k = 8; k >= 0; k--) {
+                if(chance[k] == strokeGood[k] || chance[k] == columnGood[k] || chance[k] == squareGood[k]) {
+                    delete chance[k];
+                }
+            }
+    
+            for(let l = 8; l >= 0; l--) {
+                if(chance[l] == undefined) {
+                    chance.splice(l, 1);
+                }
+            }
+    
+            if (chance.length == 1) {
+                sudoku[cell] = chance[0];
+                input[cell].value = chance[0];
             }
         }
     }
